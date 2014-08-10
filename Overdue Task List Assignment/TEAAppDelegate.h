20ff280da05a5103fc0eedf41c294a6bd90cd6d1
TonyEx <tonyangelox@Veronica.local>
2014-08-10 16:40:57 -0400

    Initial Commit

---
+++ Overdue Task List Assignment/TEAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TEAAppDelegate.h
+//  Overdue Task List Assignment
+//
+//  Created by Tony Angelo on 8/10/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Overdue Task List Assignment/TEAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TEAViewController.h
+//  Overdue Task List Assignment
+//
+//  Created by Tony Angelo on 8/10/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAViewController : UIViewController
+
+@end
